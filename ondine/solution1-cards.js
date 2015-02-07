a6e136715e825737a4aaa0ab00ff4086982604be
Ondine Gallatin <ogallatin@gmail.com>
2015-02-06 22:11:17 -0800

    Initial version from template

---
+++ ondine/solution1-cards.js
@@ -0,0 +1,78 @@
+// Homework #5.1 == Cards Module
+
+// receive factory with external name `makeCard`
+var makeCard = (function () { //begin IIFE...
+  // The factory itself:
+  function makeCard(id) {  //makeCard is also IIFE's internal name
+  // set instance properties here
+  //...
+  // and return instance...
+    if (!isValid(id))
+      return null;
+
+    return {id: id,
+      rank: makeCard.rank,
+      suit: makeCard.suit,
+      color: makeCard.color,
+      name: makeCard.cardName
+    };
+  }
+
+//--------------------------
+// Private resources (internal use only)
+//--------------------------
+
+  function isValid(id) {
+    return ((typeof id) === "number") && (id%1 === 0) && id >=0 && id <= 51;
+  }
+  var rankName = ["", "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
+  var suitName = ["", "Hearts", "Diamonds", "Spades", "Clubs"];
+
+
+//-----------------------
+// Instance Methods:
+//-----------------------
+
+  function rank() {
+    return Math.floor(this.id/4) + 1;
+  }
+
+  function suit() {
+    return (this.id%4) + 1;
+  }
+
+  function color() {
+    var suitColor = this.suit();
+    return suitColor && ((suitColor < 3) ? "red" : "black");
+  }
+
+  function cardName () {
+    var rank = this.rank();
+    var suit = this.suit();
+    return rank && suit && (makeCard.rankName[rank] + " of " + makeCard.suitName[suit]);
+  }
+
+//-----------------------
+// Factory Methods/Data:
+//-----------------------
+
+  makeCard.isCard = function(thing) {
+    return thing
+      && (typeof thing === 'object')
+      && (thing.name === makeCard.cardName)
+      && ('id' in thing) && makeCard.isValid(thing.id);
+  };
+
+  makeCard.fullSet = [];//<-- fill me
+
+  for (var id = 0; id < 52; ++id) {
+    makeCard.fullSet.push(makeCard(id));
+  }
+
+  return makeCard;  //return factory function, product of IIFE's work
+
+})(); //end IIFE definition and run it now!
+
+// Export as node-style module:
+if (typeof module != 'undefined')
+    module.exports = makeCard;
