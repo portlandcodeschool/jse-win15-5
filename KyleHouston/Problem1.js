b3a82c86cf386c6c47bd2ba77c783f10d0fdca50
KyleHouston84 <chariot124@gmail.com>
2015-02-07 20:30:32 -0800

    Problem 2 is mostly complete.

--- KyleHouston/Problem1.js
+++ KyleHouston/Problem1.js
@@ -95,3 +95,57 @@ var makeCard = // receive factory with external name `makeCard`
 // Export as node-style module:
 if (typeof module != 'undefined')
     module.exports = makeCard;
+
+//-----------------------
+// Tests:
+//-----------------------
+
+function assert(claim,message) {
+    if (!claim) console.error(message);
+}
+
+// card instances needed for assertions:
+var card0 = makeCard(0);
+var card3 = makeCard(3);
+var card5 = makeCard(5);
+var card51 = makeCard(51);
+
+// Test instance methods:
+assert(card0.rank()===1,  "Test 1 failed");
+assert(card3.rank()===1,  "Test 2 failed");
+assert(card51.rank()===13,"Test 3 failed");
+assert(card0.suit()===1,  "Test 4 failed");
+assert(card5.suit()===2,  "Test 5 failed");
+assert(card51.suit()===4, "Test 6 failed");
+assert(card0.color()==='red',   "Test 10 failed");
+assert(card3.color()==='black', "Test 11 failed");
+assert(card5.name()==='Two of Diamonds', "Test 12 failed");
+assert(card51.name()==='King of Clubs',  "Test 13 failed");
+
+// Test makeCard.isCard:
+assert(makeCard.isCard(card0),  "Test 21 failed")
+assert(makeCard.isCard(card51), "Test 22 failed")
+assert(!makeCard.isCard(0),    "Test 23 failed")
+assert(!makeCard.isCard({}),   "Test 24 failed")
+
+
+// Test failed card-making results:
+assert(!makeCard(52),"Test 26 failed");
+assert(!makeCard("0"),"Test 27 failed");
+assert(!makeCard(-1),"Test 28 failed");
+assert(!makeCard(false),"Test 30 failed");
+assert(!makeCard(true),"Test 31 failed");
+
+
+// Test fullSet array:
+assert(typeof makeCard.fullSet === 'object', "Test 40 failed");
+assert(makeCard.fullSet.length === 52, "Test 41 failed");
+assert(makeCard.isCard(makeCard.fullSet[0]), "Test 42 failed")
+assert(makeCard.fullSet[5].name() === card5.name(), "Test 43 failed");
+assert(makeCard.fullSet[51].name() === card51.name(), "Test 44 failed");
+
+// Test that methods are shared:
+assert(card0 !== card3, "Test 50 failed"); //first prove different cards
+assert(card0.rank === card3.rank, "Test 51 failed");
+assert(card0.suit === card3.suit, "Test 52 failed");
+assert(card0.name === card3.name, "Test 53 failed");
---
+++ KyleHouston/Problem2.js
@@ -0,0 +1,125 @@
+// Part a): modify factory to use non-shared (instance-specific) methods
+//  which have access to private variables:
+
+var makeDeque = (function() {
+
+	function makeDeque(values) {
+
+		// These vars are private, local to scope of makeDeque,
+		//  only accessible to functions defined in makeDeque:
+		var array = values.slice(); //copy values
+		var absent = []; //list of missing elements
+
+
+		// Each function below is specific to one deque, with access to the private vars
+
+		// ---- Internal use only ----
+		function readmit(val) {
+			return ((absent.indexOf(val) > -1)? true:false);
+		}
+
+		// ---- Public instance methods: -----
+
+		function top() {
+			return array[array.length - 1];
+		}
+
+		function bottom() {
+			return array[0];
+		}
+
+		function myLength() {
+			return array.length;
+		}
+
+		function pop() {
+			var popCard = array.pop();
+			absent.push(popCard);
+
+			return popCard;
+		}
+
+		function push(val) {
+			var index = absent.indexOf(val);
+
+			if (this.readmit(val)) {
+				absent.splice(index,1);
+				return array.push(val);
+			} else {
+				return null;
+			}
+		}
+
+		function shift() {
+			var shiftCard = array.shift();
+			absent.push(shiftCard);
+
+			return shiftCard;
+		}
+
+		function unshift(val) {
+			var index = absent.indexOf(val);
+
+			if (index > -1) {
+				absent.splice(index,1);
+				return array.unshift(val);
+			} else {
+				return null;
+			}
+		}
+
+		function cut() {
+			var cutPoint = Math.ceil(array.length/2);
+			var tempArrayBottom = [],
+				tempArrayTop = [];
+
+			tempArrayBottom = array.slice(0,cutPoint);
+			tempArrayTop = array.slice(cutPoint, array.length);
+
+			array = tempArrayTop.concat(tempArrayBottom);
+			return array;
+		}
+
+		function map(convertValFn) {
+			return array.map(convertValFn);
+		}
+
+		function sort(compareValsFn) {
+			return array.sort(compareValsFn);
+		}
+
+		function shuffle(shuffleFn) {
+			return array.sort(shuffleFn);
+		}
+		// etc...
+
+		function check() {
+			console.log("Absent: " + absent);
+			console.log("Array: " + array);
+		}
+
+
+		return { //one deque instance...
+				length: myLength,
+				top: top,
+				bottom: bottom,
+				pop: pop,
+				push: push,
+				shift: shift,
+				unshift: unshift,
+				cut: cut,
+				map: map,
+				sort: sort,
+				shuffle: shuffle,
+				check: check,
+				readmit: readmit,
+		};
+	}
+
+	return makeDeque;
+})(); //end makeDeque
+
+var x = [1, 2, 3, 4, 5, 6, 7];
+//var deck = makeDeque(makeCard.fullSet);
+var deck = makeDeque(x);
+// Part b): Turn this file into an IIFE module!
