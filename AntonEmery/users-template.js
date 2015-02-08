cddcbc7d6fbbdc50adfd0cc6518300fe601b618a
AntonEmery <antonemery@gmail.com>
2015-02-07 16:14:19 -0800

    users template

--- AntonEmery/users-template.js
+++ AntonEmery/users-template.js
@@ -1,38 +1,43 @@
-var makeUser = (function() {// begin IIFE...
+// var makeUser = (function() {// begin IIFE...
 	
-	var sharedLog = [];  //private; accessible only from functions defined within IIFE
+// 	var sharedLog = [];  //private; accessible only from functions defined within IIFE
 
 
 	// The factory itself:
-	function makeUser(name,passwd) {
+		function makeUser(name,passwd) {
 
-		var getName = name;
-		var getPasswd = passwd;
-		// Return a user object with three methods:
-		var user = {
-			// getName()
-			username : function(name){  //setters
-				return name;
-			},
-			// sett password
-			password : function(passwd) { //setters
-				return passwd;
-			},
-			//need getters
-			getName : function() {
-				return name;
-			},
-			getPasswd : function() {
-				return password;
+			// Return a user object with three methods:
+			var user = {
+				// getName()
+				username : function(name){  //setters
+					return name;
+				},
+				// set password
+				password : function(passwd) { //setters
+					return passwd;
+				},
+				//getters
+				getName : function() {
+					return name;
+				},
+				getPasswd : function() {
+					return password;
+				},
+
+				//validate string
+				validate : function(str) {
+				(password === str ? true : false);
+				}
+
+		/*	// record(message) (Part b)
+				var publicpassword = password;  */
 			}
-
-			//validate string
-
-		// record(message) (Part b)
-			var publicpassword = password;
 		};
-	}
 
+
+
+
+/*
 	// Part b) only:
 	// Factory method (defined within IIFE, so can access sharedLog):
 	makeUser.getLog = function(user) {
@@ -43,3 +48,4 @@ var makeUser = (function() {// begin IIFE...
 
 if (typeof module != 'undefined')
 	module.exports = makeUser;
+*/
