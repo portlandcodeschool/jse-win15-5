3be464a2eb3e946b5d14dc13649c0b8278f5cfca
Greg Borchers <greg.borchers@comcast.net>
2015-02-08 23:02:04 -0800

    HW5 Problem1 now passing tests

---
+++ gregBorchers/HW5Problem1.js
@@ -0,0 +1,198 @@
+var makeCard = // receive factory with external name `makeCard`
+    (function() { //begin IIFE...
+
+        // The factory itself:
+        function makeCard(id) { //makeCard is also IIFE's internal name
+            // set instance properties here
+            makeCard.fullSet = [];
+            // If id is invalid (out of range, etc)
+            if (makeCard.isValidID(id)) {
+                // **** properties of each card
+                var theNewCard = {};
+                theNewCard.id = id;
+                theNewCard.rank = makeCard.rank;
+                theNewCard.suit = makeCard.suit;
+                theNewCard.color = makeCard.color;
+                theNewCard.name = makeCard.getCardName;
+                // ****
+                return theNewCard;
+            } else {
+                return null; // bad ID
+            }
+        };
+        // and return instance...
+
+
+        //--------------------------
+        // Private resources (internal use only)
+        //--------------------------
+
+        // Examples:
+
+        //function isValidID(num) {...}
+        makeCard.isValidID = function(id) {
+            // validate inputs
+            if (!((typeof id === 'number') && (id % 1 === 0) // is integer
+                && id < 52 // is in range
+                && id >= 0)) // is in range
+            {
+                return false; // INPUT ERROR
+            } else {
+                return true;
+            }
+        };
+
+        makeCard.makeFullSet = function(cardSet) {
+            for (var i = 0; i < 52; i++) {
+                cardSet[i] = makeCard(i);
+            }
+            makeCard.printFullSet(cardSet);
+        };
+        makeCard.printFullSet = function(fullSet) {
+            // verbose output
+            // TODO add some error checking
+            //console.log("< ****** --> START of call to makeCard.makeFullSet() **** ");
+            console.log("Here is the deck **** >");
+            for (var i = 0; i < fullSet.length; i++) {
+                console.log("Card ID=" + i + " name() returned: " + fullSet[i].name());
+            }
+            //console.log("********* --> END of call to makeCard.makeFullSet() **** >");
+        };
+
+        //var rankNames = [...];
+        var suitNames = ["Hearts", "Diamonds", "Spades", "Clubs"];
+        var rankNames = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
+
+
+        //-----------------------
+        // Instance Methods:
+        //-----------------------
+
+        makeCard.rank = function() { // --> 1..13, NaN
+            return Math.trunc(this.id / 4) + 1;
+        };
+        makeCard.suit = function() { // --> 1..4, NaN
+            return (this.id % 4) + 1;
+        };
+        makeCard.color = function() { // -->"red,"black",NaN
+            var cardColor = "";
+            if ((this.suit(this.id) === 0) || (this.suit(this.id) === 1)) {
+                cardColor = "red";
+            } else {
+                cardColor = "black";
+            };
+            return cardColor;
+        };
+        makeCard.getCardName = function() { //--> string, NaN
+            var cardName = "";
+            cardName = rankNames[this.rank(this.id) - 1] + " of " + suitNames[this.suit(this.id) - 1];
+            return cardName;
+        };
+        //etc...
+
+
+        //-----------------------
+        // Factory Methods/Data:
+        //-----------------------
+        makeCard.isCard = function(cardObj) {
+            // TODO look for more authoritative methods that would be a higher bar to validate (i.e. security standards)
+            // However, a simple "quacks like a duck==is duck-ish" test will work, so just enumarate the methods and validate the id.
+
+            if (typeof cardObj === 'undefined') {
+                return false;
+            } // basic Garbage-in test
+
+            var isACard = true;
+            if (!(makeCard.isValidID(cardObj.id))) {
+                isACard = false;
+            };
+            if (isACard && // TODO add more robust validations here
+                !(('rank' in cardObj) &&
+                    ('suit' in cardObj) &&
+                    ('color' in cardObj) &&
+                    ('name' in cardObj))) {
+                isACard = false;
+            };
+            return isACard;
+        };
+
+
+
+
+        // full set of cards (per factory)
+        makeCard.fullSet = []; //<-- fill me
+        makeCard.makeFullSet(makeCard.fullSet);
+
+        return makeCard; //return factory function, product of IIFE's work
+
+    })(); //end IIFE definition and run it now!
+
+console.log(makeCard);
+
+
+// Export as node-style module:
+if (typeof module != 'undefined')
+    module.exports = makeCard;
+
+
+//----------------------
+// Simple Testing suite
+// Supplement as needed!
+function assert(claim, message) {
+    if (!claim) console.error(message);
+}
+// create a factory
+
+// card instances needed for assertions:
+console.log("\n-START------ card instances needed for assertions");
+
+var card0 = makeCard(0);
+var card3 = makeCard(3);
+var card5 = makeCard(5);
+var card51 = makeCard(51);
+// Test instance methods:
+
+console.log("-END-------- \n");
+
+
+console.log("\n-START-------- starting rank, suit, color, name tests");
+assert(card0.rank() === 1, "Test 1 failed");
+assert(card3.rank() === 1, "Test 2 failed");
+assert(card51.rank() === 13, "Test 3 failed");
+assert(card0.suit() === 1, "Test 4 failed");
+assert(card5.suit() === 2, "Test 5 failed");
+assert(card51.suit() === 4, "Test 6 failed");
+assert(card0.color() === 'red', "Test 10 failed");
+assert(card3.color() === 'black', "Test 11 failed");
+assert(card5.name() === 'Two of Diamonds', "Test 12 failed");
+assert(card51.name() === 'King of Clubs', "Test 13 failed");
+console.log("-END-------- \n");
+console.log("\n-START-------- starting Test makeCard.isCard & makeCard");
+// Test makeCard.isCard:
+assert(makeCard.isCard(card0), "Test 21 failed");
+assert(makeCard.isCard(card51), "Test 22 failed");
+assert(!makeCard.isCard(0), "Test 23 failed");
+assert(!makeCard.isCard({}), "Test 24 failed");
+// Test failed card-making results:
+assert(!makeCard(52), "Test 26 failed");
+assert(!makeCard("0"), "Test 27 failed");
+assert(!makeCard(-1), "Test 28 failed");
+assert(!makeCard(false), "Test 30 failed");
+assert(!makeCard(true), "Test 31 failed");
+console.log("-END-------- \n");
+console.log("\n-START-------- starting Test fullset factory array");
+// // Test fullSet array:
+// cardSet must be made externally, not like in HW4 where we could get the internal fullDeck out. (it won't come to play anymore!)
+var cardSet = [];
+makeCard.makeFullSet(cardSet);
+assert(cardSet.length === 52, "Test 41 failed");
+assert(makeCard.isCard(cardSet[0]), "Test 42 failed")
+assert(cardSet[5].name() === card5.name(), "Test 43 failed");
+assert(cardSet[51].name() === card51.name(), "Test 44 failed");
+// Test that methods are shared:
+assert(card0 !== card3, "Test 50 failed"); //first prove different cards
+assert(card0.rank === card3.rank, "Test 51 failed");
+assert(card0.suit === card3.suit, "Test 52 failed");
+assert(card0.name === card3.name, "Test 53 failed");
+console.log("-END-------- \n");
+// //etc...
