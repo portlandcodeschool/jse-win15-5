9976b0819092be1aa2c9fc88eaef92a9e8257c7f
Tal Zivelin <tzivelin@tzivelin.local>
2015-02-08 20:28:22 -0800

    I can't seem to get my stupid branching right

--- TalZivelin/cards4-template.js
+++ TalZivelin/cards4-template.js
@@ -3,9 +3,20 @@ var makeCard = // receive factory with external name `makeCard`
 
     // The factory itself:
     function makeCard(id) {  //makeCard is also IIFE's internal name
-        // set instance properties here
-        //...
-        // and return instance...
+       if (!(Number.isInteger(id)) || id < 0 || id > 51 ) {
+        return null;
+    }
+		
+	 var card = {
+		id: id,
+		rank : rank,
+		suit : suit,
+		color : color,
+		cardName : cardName
+	  }
+  
+  return card
+  
     };
 
 
@@ -17,16 +28,33 @@ var makeCard = // receive factory with external name `makeCard`
 
     //function isValidID(num) {...}
     //var rankNames = [...];
-
+	
+	 var rankNames = ['','Ace','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten',
+                'Jack','Queen','King'];
+      var suitNames = ['','Hearts','Diamonds','Spade','Clubs'];
+	
+	 
 //-----------------------
 // Instance Methods:
 //-----------------------
 
-    function rank() {
+    function rank() {		
+		return  Math.floor(this.id/4)+1
     }
 
     function suit() {
+		return (this.id%4)+1
     }
+	
+	function color() {
+	  return (this.suit(this.id) < 3)? "red": "black"
+	};
+	
+	
+	function cardName() {
+	 	return rankNames[this.rank(this.id)] + ' of ' + suitNames[this.suit(this.id)];
+	};
+
 
     //etc...
 
@@ -35,11 +63,18 @@ var makeCard = // receive factory with external name `makeCard`
 // Factory Methods/Data:
 //-----------------------
 
-    makeCard.isCard = function(thing) {
+    makeCard.isCard = function(id) { // --> true,false
+    // return true if thing is a valid card instance made by this factory
+	if (id.name == makeCard.cardName)
+	 {
+    	return true;
+  }
+}
 
-    };
-
-    makeCard.fullSet = [];//<-- fill me
+   makeCard.fullSet = []; //<-- instead, generate array of 52 card instances
+	for (var i = 0; i < 52; i++){
+		makeCard.fullSet.push(makeCard(i));
+	}
 
 
 
@@ -50,3 +85,8 @@ var makeCard = // receive factory with external name `makeCard`
 // Export as node-style module:
 if (typeof module != 'undefined')
     module.exports = makeCard;
+	
+
+
+console.log("This function is an IIFE that can be run by calling makeCard")
+
--- TalZivelin/users-template.js
+++ TalZivelin/users-template.js
@@ -5,20 +5,61 @@ var makeUser = (function() {// begin IIFE...
 
 	// The factory itself:
 	function makeUser(name,passwd) {
+
+		
+		var user	 = {
+			name : name,
+			passwd : passwd,
+      
+			getName : function(){
+				return this.name;
+			},
+      
+      		validate : function(str){
+				return (this.passwd === str);
+			}
+      
+		}
+    
+
+		
+		return user
+    
+    
 		// Return a user object with three methods:
 		// getName()
 		// validate(str)
 		// record(message) (Part b)
 	}
+	
+	
+	
 
 	// Part b) only:
 	// Factory method (defined within IIFE, so can access sharedLog):
-	makeUser.getLog = function(user) {
-	}
+	/*makeUser.getLog = function(user) {
+	}*/
 
 	return makeUser;
 })();
 
 
-if (typeof module != 'undefined')
+/*if (typeof module != 'undefined')
 	module.exports = makeUser;
+
+
+function storePassword(passwd) {
+   
+  return function(guess){
+   
+    return (guess === passwd);
+  } 
+  
+};*/
+
+User01 = makeUser("bob", "pass");
+User01.validate("pass")
+
+console.log("this is the getName function " + User01.getName());
+console.log("this is the validate function correct password " + User01.validate("pass"));
+console.log("this is the validate function incorrect password " + User01.validate("passs"))
